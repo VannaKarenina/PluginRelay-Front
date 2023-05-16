50d98e73df1b8d384a0ca72931509a861d0c06a1
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-05-17 00:02:35 +0300

    Defined per game page [WIP]

--- pages/games/[id].vue
+++ pages/games/[id].vue
@@ -1,13 +1,47 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import Modal from "~/components/base/Modal.vue";
 
 export default defineComponent({
-name: "[id]"
+    name: "[id]",
+    components: {Modal},
+    setup() {
+      const route = useRoute();
+
+      const showModal = ref(false);
+      function openModal() {
+          showModal.value = true;
+      }
+      function closeModal() {
+          showModal.value = false;
+      }
+
+      return {
+          route,
+          showModal,
+          openModal,
+          closeModal,
+      };
+    },
+    data() {
+        return {
+          projectId: this.route.params.id,
+          modal: false
+        }
+    },
+    methods: {
+
+    }
 })
 </script>
 
 <template>
-  $END$
+  <div class="tw-h-screen">
+      <button @click="openModal">Open Modal</button>
+      <Modal v-if="showModal" @close="closeModal">
+          kek2222222
+      </Modal>
+  </div>
 </template>
 
 <style scoped>
