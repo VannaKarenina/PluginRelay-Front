46dfd46da28ea44ad8f58d81bc8b883b783ebc97
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-05-17 00:02:16 +0300

    Main page rework

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,20 @@ import {defineComponent} from 'vue'
 import card from "~/components/card.vue";
 import CategoriesView from "~/components/view/CategoriesView.vue";
 import CallToAction from "~/components/base/CallToAction.vue";
+import LatestGames from "~/components/view/LatestGames.vue";
 
 export default defineComponent({
     name: "index",
     components: {
         CallToAction,
-        CategoriesView,
+        LatestGames,
     },
 })
 </script>
 
 <template>
     <div class="tw-min-h-screen">
-        <CategoriesView/>
+        <LatestGames/>
         <CallToAction/>
     </div>
 </template>
