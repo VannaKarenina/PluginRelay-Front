86061b542d0df3f7170ad58722106ef4d704819b
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-05-17 00:00:02 +0300

    Base modal component

---
+++ components/base/Modal.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "Modal",
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        },
+    },
+})
+</script>
+
+<template>
+    <div class="tw-fixed tw-inset-0 tw-flex tw-items-center tw-justify-center tw-z-50">
+        <div class="tw-bg-white tw-rounded-lg tw-p-4 lg:tw-w-300 lg:tw-h-200 sm:tw-w-100 tw-z-50">
+            <slot></slot>
+        </div>
+        <div class="tw-fixed tw-inset-0 tw-bg-black tw-opacity-50" @click="closeModal"></div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/games/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import AllGames from "~/components/view/AllGames.vue";
+
+export default defineComponent({
+    name: "games",
+    components: {AllGames}
+})
+</script>
+
+<template>
+  <div class="tw-min-h-screen">
+      <AllGames/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
