b3f34a8073c15b00c4cd53b8c9e0b5bc6bf73372
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-05-21 00:43:44 +0300

    Projects page implemented [78%]

--- pages/categories/[id].vue
+++ pages/categories/[id].vue
@@ -1,49 +1,63 @@
+<template>
+  <div class="tw-min-h-screen">
+    <div class="tw-flex tw-justify-center">
+      <div>All modifications in &nbsp;</div>
+      <div>{{category.name}}</div>
+    </div>
+    <div class="tw-flex tw-justify-center">
+      <div class="tw-grid tw-grid-cols-1 sm:tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-2 tw-gap-4 tw-mt-5">
+        <!-- Loop through projects -->
+        <div v-for="(project, index) in category.projects" :key="index">
+          <projects-card :project="project"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, ref} from 'vue'
 import Modal from "~/components/base/Modal.vue";
+import axios from "axios";
+import {useRoute} from "#app";
 
-export default defineComponent({
-    name: "[id]",
-    components: {Modal},
-    setup() {
-      const route = useRoute();
+export default defineComponent(
+    {
+      name: "[id]",
+      components: {Modal},
+      async setup() {
+        const route = useRoute();
 
-      const showModal = ref(false);
-      function openModal() {
+        const showModal = ref(false);
+        function openModal() {
           showModal.value = true;
-      }
-      function closeModal() {
+        }
+        function closeModal() {
           showModal.value = false;
-      }
+        }
 
-      return {
+        return {
           route,
           showModal,
           openModal,
           closeModal,
-      };
-    },
-    data() {
+        };
+      },
+      data() {
         return {
           projectId: this.route.params.id,
           modal: false,
-          category: null
+          image: 'http://127.0.0.1:3890/v1/storage/getCategoryAvatar?key=wl.png',
+          category: {} as object
         }
-    },
-    async mounted() {
-      this.category = await $fetch(`http://127.0.0.1:3890/v1/category/${this.projectId}`);
-    },
-    methods: {
-
+      },
+      async beforeMount() {
+        this.category = await $fetch(`http://127.0.0.1:3890/v1/category/${this.projectId}`);
+        console.log(this.category)
+      }
     }
-})
+)
 </script>
-
-<template>
-  <div class="tw-h-screen">
-
-  </div>
-</template>
 
 <style scoped>
 
