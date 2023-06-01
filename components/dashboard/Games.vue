b720142911905137fe87d61d0dc11417dc02285a
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-06-01 23:24:57 +0300

    Some ui fixes

---
+++ components/dashboard/Games.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="tw-min-h-screen tw-ml-5">
+    rtr
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/user/dashboard.vue
+++ pages/user/dashboard.vue
@@ -1,53 +1,49 @@
-<script lang="ts">
+<script setup lang="ts">
 import {useAuthStore} from "~/stores/auth";
 import SideNav from "~/components/base/SideNav.vue";
 import {$fetch} from "ofetch";
 import Projects from "~/components/dashboard/Projects.vue";
-export default {
-  components: {SideNav, Projects},
-  async setup() {
-    definePageMeta({
-      middleware: [
-        "auth"
-      ]
-    })
-    const user = ref({});
-    const store = useAuthStore();
-    onMounted(async () => {
+
+definePageMeta({
+  middleware: [
+    "auth"
+  ]
+})
+
+const store = useAuthStore();
+const state = ref(0);
+
+const {data: user} = await useAsyncData(
+    'user',
+    () => {
       try {
-        user.value = await $fetch('http://127.0.0.1:3890/v1/account/identity', {
+        return $fetch('http://127.0.0.1:3890/v1/account/identity', {
           headers: {
             Authorization: `Bearer ${store.token}`
           }
-        })
+        });
       } catch (error) {
         if (error.response && error.response.status === 401) {
           store.logout()
-          return navigateTo('/')
+          navigateTo('/')
         } else {
           console.error(error);
         }
       }
-    })
-    return {store, user}
-  },
-  data() {
-    return {
-      state: 0
     }
-  },
-  methods: {
-    changeState(e) {
-      switch (e) {
-        case 0:
-          this.state = 0;
-          break;
-        case 1:
-          this.state = 1;
-          break;
 
-      }
-    },
+
+)
+
+function changeState(e) {
+  switch (e) {
+    case 0:
+      state.value = 0;
+      break;
+    case 1:
+      state.value = 1;
+      break;
+
   }
 }
 </script>
@@ -55,7 +51,30 @@ export default {
 <template>
   <div class="tw-flex tw-flex-row tw-min-h-screen">
     <SideNav :account="user" @state="changeState" />
+    <div v-if="state == 0" class="tw-w-screen tw-flex tw-flex-row tw-justify-around tw-items-center mb-16">
+      <div class="tw-w-60 tw-h-60 tw-bg-gray-700 tw-flex tw-rounded-lg tw-items-center tw-justify-center">
+        <div class="tw-justify-self-center tw-text-center">
+          <div class="tw-text-xl tw-text-white">
+            {{user.projects.length}}
+          </div>
+          <div class="tw-text-xl tw-text-white">
+            PROJECTS
+          </div>
+        </div>
+      </div>
+      <div class="tw-w-60 tw-h-60 tw-bg-gray-700 tw-flex tw-rounded-lg tw-items-center tw-justify-center">
+        <div class="tw-justify-self-center tw-text-center">
+          <div class="tw-text-xl tw-text-white">
+            {{user.projects.reduce((total, obj) => total + obj.downloads, 0)}}
+          </div>
+          <div class="tw-text-xl tw-text-white">
+            DOWNLOADS
+          </div>
+        </div>
+      </div>
+    </div>
     <Projects v-if="state == 1" />
+    <Games v-if="user.moderation_level >= 1 && state == 3"/>
   </div>
 </template>
 
